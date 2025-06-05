75749c8601b3d8aa485621fd9d9d307f487f20a9
nikita <nikitamalihin123@gmail.com>
2025-06-05 23:46:11 +0300

    second

--- Homeworks/132123.py
+++ Homeworks/132123.py
@@ -13,3 +13,14 @@ for task in goals:
     else:
         task_line.append(task)
 print(task_line)
+
+
+def count_up_to(max):
+    count = 1
+    while count <= max:
+        yield count
+        count += 1
+
+
+for num in count_up_to(5):
+    print(num)
