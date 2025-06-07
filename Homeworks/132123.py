b42439e254d84e10aa3781444e00114122ad5def
nikita <nikitamalihin123@gmail.com>
2025-06-07 23:51:52 +0300

    foutrth

--- Homeworks/132123.py
+++ Homeworks/132123.py
@@ -25,3 +25,18 @@ def count_up_to(max):
 for num in count_up_to(10):
     print(num)
     print(max)
+
+def complicated(x: int, y: int) -> int:
+    return x + y
+
+def logger(func):
+    def inner(x: int, y: int) -> int:
+        print(f"Викликається функція: {func.__name__}: {x}, {y}")
+        result = func(x, y)
+        print(f"Функція {func.__name__} завершила виконання: {result}")
+        return result
+
+    return inner
+
+complicated = logger(complicated)
+print(complicated(2, 3))
