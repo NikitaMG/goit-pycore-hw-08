f0856090e7d5ef4d24ea9375926660c8f7fa5d96
nikita <nikitamalihin123@gmail.com>
2025-06-08 23:28:48 +0300

    any

--- Homeworks/132123.py
+++ Homeworks/132123.py
@@ -51,3 +51,6 @@ print(squares)  # [1, 4, 9, 16]
 numbers = [1, 2, 3, 4, 5, 6]
 even = list(filter(lambda x: x % 2 == 0, numbers))
 print(even)  # 👉 [2, 4, 6]
+
+values = [0, 0, 1, 0]
+print(any(values))  # 👉 True
